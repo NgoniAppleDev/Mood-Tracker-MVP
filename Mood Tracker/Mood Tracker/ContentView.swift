badc8ea766d575b0fbf83b52c1c18ddb8a9a92f2
NgoniAppleDev <ngonikatsidzira@icloud.com>
2025-01-28 04:55:30 +0200

    initial xcode changes

---
+++ Mood Tracker/Mood Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mood Tracker
+//
+//  Created by Ngoni Katsidzira  on 28/1/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Coding Swiftly!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mood Tracker/Mood Tracker/Mood_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mood_TrackerApp.swift
+//  Mood Tracker
+//
+//  Created by Ngoni Katsidzira  on 28/1/2025.
+//
+
+import SwiftUI
+
+@main
+struct Mood_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
